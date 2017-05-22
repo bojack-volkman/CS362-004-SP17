252962c670db91959437e6e9a28da779829d6eb7
aoleson <alannah313@gmail.com>
2017-05-22 11:14:54 -0700

    added helper function for generating URL strings

--- FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -59,10 +59,30 @@ public class UrlValidatorTest extends TestCase {
    
    public void testIsValid()
    {
+	   String myURL = makeURLStringToTest(0, 0, 0, 0, 0, 0);
+	   System.out.println(myURL);	//Debug
+	   
+	   
 	   for(int i = 0;i<10000;i++)
 	   {
 		   
 	   }
+   }
+   
+   public String  makeURLStringToTest (int scheme_index, int auth_index, int port_index, int path_index, int path_option, int query_index) {
+   
+	   String scheme_choice = test_schemes[scheme_index].item;
+	   String auth_choice = test_auths[auth_index].item;
+	   String port_choice = test_ports[port_index].item;
+	   String path_choice = test_paths[path_index].item;
+	   String option_choice = test_path_options[path_option].item;
+	   String query_choice = test_queries[query_index].item;
+	   
+	   //Debug
+	   System.out.println(scheme_choice + auth_choice + port_choice + path_choice + option_choice + query_choice);
+	   
+	   return (scheme_choice + auth_choice + port_choice + path_choice + option_choice + query_choice);
+	   
    }
    
    public void testAnyOtherUnitTest()
@@ -77,4 +97,81 @@ public class UrlValidatorTest extends TestCase {
     */
    
 
+   /*************************************************
+    * The following arrays are from the given URL validator tests. We 
+    * include these to ensure we have a testable input set that covers
+    * a large amount of cases and to ensure that we cover at least as much
+    * of the cases as the orginal test.
+    ************************************************/
+   ResultPair[] test_schemes = {new ResultPair("http://", true),
+           new ResultPair("ftp://", true),
+           new ResultPair("h3t://", true),
+           new ResultPair("3ht://", false),
+           new ResultPair("http:/", false),
+           new ResultPair("http:", false),
+           new ResultPair("http/", false),
+           new ResultPair("://", false),
+           new ResultPair("", true)
+   };
+   ResultPair[] test_auths = {new ResultPair("www.google.com", true),
+           new ResultPair("go.com", true),
+           new ResultPair("go.au", true),
+           new ResultPair("0.0.0.0", true),
+           new ResultPair("255.255.255.255", true),
+           new ResultPair("256.256.256.256", false),
+           new ResultPair("255.com", true),
+           new ResultPair("1.2.3.4.5", false),
+           new ResultPair("1.2.3.4.", false),
+           new ResultPair("1.2.3", false),
+           new ResultPair(".1.2.3.4", false),
+           new ResultPair("go.a", false),
+           new ResultPair("go.a1a", false),
+           new ResultPair("go.cc", true),
+           new ResultPair("go.1aa", false),
+           new ResultPair("aaa.", false),
+           new ResultPair(".aaa", false),
+           new ResultPair("aaa", false),
+           new ResultPair("", false)
+   };
+   ResultPair[] test_ports = {new ResultPair(":80", true),
+           new ResultPair(":65535", true),
+           new ResultPair(":0", true),
+           new ResultPair("", true),
+           new ResultPair(":-1", false),
+           new ResultPair(":65636", true),
+           new ResultPair(":65a", false)
+   };
+   ResultPair[] test_paths = {new ResultPair("/test1", true),
+           new ResultPair("/t123", true),
+           new ResultPair("/$23", true),
+           new ResultPair("/..", false),
+           new ResultPair("/../", false),
+           new ResultPair("/test1/", true),
+           new ResultPair("", true),
+           new ResultPair("/test1/file", true),
+           new ResultPair("/..//file", false),
+           new ResultPair("/test1//file", false)
+   };
+   ResultPair[] test_path_options = {new ResultPair("/test1", true),
+           new ResultPair("/t123", true),
+           new ResultPair("/$23", true),
+           new ResultPair("/..", false),
+           new ResultPair("/../", false),
+           new ResultPair("/test1/", true),
+           new ResultPair("/#", false),
+           new ResultPair("", true),
+           new ResultPair("/test1/file", true),
+           new ResultPair("/t123/file", true),
+           new ResultPair("/$23/file", true),
+           new ResultPair("/../file", false),
+           new ResultPair("/..//file", false),
+           new ResultPair("/test1//file", true),
+           new ResultPair("/#/file", false)
+   };
+   ResultPair[] test_queries = {new ResultPair("?action=view", true),
+           new ResultPair("?action=edit&mode=up", true),
+           new ResultPair("", true)
+   };
+   
+   
 }
